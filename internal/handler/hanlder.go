0844b1ee0041c59e239e88bbc36358572306aa03
Igor-Makeev <Igormakeev2017@gmail.com>
2022-12-02 04:47:45 +1000

    clean comments

--- internal/handler/hanlder.go
+++ internal/handler/hanlder.go
@@ -27,38 +27,3 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	return router
 }
-
-// func (h *Handler) RootHandler(w http.ResponseWriter, req *http.Request) {
-// 	if req.URL.Path == "/" {
-
-// 		switch req.Method {
-// 		case http.MethodPost:
-// 			h.PostHandler(w, req)
-
-// 		default:
-// 			http.Error(w, fmt.Sprintf("expect method POST at /, got %v", req.Method), http.StatusMethodNotAllowed)
-// 			return
-// 		}
-
-// 	} else {
-// 		switch req.Method {
-// 		case http.MethodGet:
-
-// 			pathParts := strings.Split(req.URL.Path, "/")
-
-// 			if len(pathParts) < 2 {
-// 				http.Error(w, "expect /<id> in  GetURLHandler handler", http.StatusBadRequest)
-// 				return
-// 			}
-// 			id, err := strconv.Atoi(pathParts[1])
-// 			if err != nil {
-// 				http.Error(w, err.Error(), http.StatusBadRequest)
-// 				return
-// 			}
-// 			h.GetURLHandler(w, req, id)
-// 		default:
-// 			http.Error(w, fmt.Sprintf("expect method GET at /{id}, got %v", req.Method), http.StatusMethodNotAllowed)
-// 			return
-// 		}
-// 	}
-// }

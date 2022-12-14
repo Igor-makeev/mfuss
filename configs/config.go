faca404c64699cf43c9513453fe2c7469c523110
Igor-Makeev <Igormakeev2017@gmail.com>
2022-12-15 05:07:15 +1000

    config changes

--- configs/config.go
+++ configs/config.go
@@ -3,5 +3,5 @@ package configs
 type Config struct {
 	SrvAddr         string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"file_storage.txt"`
 }
